bcc6572ceefaa687c0579491eed5dda5512c410f
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-05-24 19:21:21 -0500

    fix js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ class Book {
   constructor(title, author, id) {
     this.title = title;
     this.author = author;
-     this.id = id;
+    this.id = id;
   }
 }
-
 
 const list = document.querySelector('#list');
 
@@ -15,7 +14,7 @@ class UI {
     items.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
-    <td><button class="remove-btn">Remove</button></td> 
+    <td><button data-id="${book.id}" class="remove-btn">Remove</button></td> 
     `;
 
     list.appendChild(items);
@@ -33,7 +32,6 @@ class UI {
 }
 
 class Store {
-  //method gets the book from local storage
   static getBooks() {
     let books = [];
     if (localStorage.getItem('books') === null) {
@@ -44,29 +42,25 @@ class Store {
     return books;
   }
 
-  //method adds the book
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  //method Remove the Bood
-  static removeBook(title) {
+  static removeBook(ID) {
     const books = Store.getBooks();
     books.forEach((book, index) => {
-      if (book.title === title) {
+      if (book.id === Number(ID)) {
         books.splice(index, 1);
       }
     });
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  //method generated ID
-  static generateId(){
+  static generateId() {
     return Math.floor(Math.random() * 100000000);
   }
-
 }
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -81,11 +75,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 document.querySelector('#list').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
-  const text = e.target.parentElement.previousElementSibling.previousElementSibling;
-  Store.removeBook(text.textContent);
+  const ID = e.target.getAttribute('data-id');
+  Store.removeBook(ID);
 });
 
-// init app
 function init() {
   list.innerHTML = '';
   Store.getBooks().forEach((book) => UI.getAllBooks(book));
